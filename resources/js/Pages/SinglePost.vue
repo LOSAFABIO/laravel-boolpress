74dbe93792d90e4c4899032ec855b1d89ae33be0
LOSAFABIO <fabio.losa95@gmail.com>
2022-03-25 18:25:23 +0100

    finito e sistemato un po css

--- resources/js/Pages/SinglePost.vue
+++ resources/js/Pages/SinglePost.vue
@@ -1,21 +1,23 @@
 <template>
-    <div>
-        <h1>Titoli POST</h1>
-        <h3>Titolo</h3>
-        <p>{{post.title}}</p>
-        <h3>Contenuto</h3>
-        <p>{{post.content}}</p>
-        <h3>Data Post</h3>
-        <p>{{post.post_date}}</p>
-        <h3>Autore</h3>
-        <p>{{post.author}}</p>
-        <h3>Slug</h3>
-        <p>{{post.slug}}</p>
-        <h3>Categoria</h3>
-        <p>{{post.category ? post.category.name : "Nessuna categoria"}}</p>
-        <h3>Tags</h3>
-        <p v-for="tag in post.tags" :key="tag.id">{{tag.name}}</p>
-        <!-- <router-link :to="{ name: 'single-post', params: {slug: 'aggiunto nuovo' }}">Visualizza IL Post</router-link> -->
+    <div class="container">
+        <div class="box visualizza">
+            <h1>Post Singolo</h1>
+            <h3>Titolo</h3>
+            <p>{{post.title}}</p>
+            <h3>Contenuto</h3>
+            <p>{{post.content}}</p>
+            <h3>Data Post</h3>
+            <p>{{post.post_date}}</p>
+            <h3>Autore</h3>
+            <p>{{post.author}}</p>
+            <h3>Slug</h3>
+            <p>{{post.slug}}</p>
+            <h3>Categoria</h3>
+            <p>{{post.category ? post.category.name : "Nessuna categoria"}}</p>
+            <h3>Tags</h3>
+            <p v-for="tag in post.tags" :key="tag.id">{{tag.name}}</p>
+            <!-- <router-link :to="{ name: 'single-post', params: {slug: 'aggiunto nuovo' }}">Visualizza IL Post</router-link> -->
+        </div>
     </div>
 </template>
 
@@ -41,5 +43,31 @@ export default ({
 </script>
 
 <style scoped>
+
+.container{
+    width: 100vw;
+    height: 100vh;
+}
+
+.box{
+    border: solid 1px black;
+    border-radius: 10px;
+    width: 400px;
+    margin: 6px;
+    padding: 40px;
+    list-style-type: none;
+}
+
+h3, p{
+    text-align: center;
+}
+.visualizza{
+    border: solid 1px black;
+    border-radius: 10px;
+    margin: 0 auto;
+    margin-top: 55px;
+    text-align: center;
+    background-color: rgba(167, 80, 80, 0.4)
+}
 
 </style>
--- resources/js/components/Posts.vue
+++ resources/js/components/Posts.vue
@@ -1,18 +1,28 @@
 <template>
-    <div>
+    <div class="container">
         <h1>Titoli POST</h1>
-        <ol>
-            <li v-for="(post) in posts" :key="post.id">
-                <h1>Titolo</h1>{{post.title}}
-                <h1>Contenuto</h1>{{post.content}}
-                <h1>Data Post</h1>{{post.post_date}}
-                <h1>Autore</h1>{{post.author}}
-                <h1>Slug</h1>{{post.slug}}
+        <ol class="my_flex">
+            <li 
+            v-for="(post) in posts" 
+            :key="post.id"
+            class="box">
+                <h1>Titolo</h1>
+                <p>{{post.title}}</p>
+                <h1>Contenuto</h1>
+                <p>{{post.content}}</p>
+                <h1>Data Post</h1>
+                <p>{{post.post_date}}</p>
+                <h1>Autore</h1>
+                <p>{{post.author}}</p>
+                <h1>Slug</h1>
+                <p>{{post.slug}}</p>
                 <h1>Categoria</h1>
                 <p>{{post.category ? post.category.name : "Nessuna categoria"}}</p>
                 <h1>Tags</h1>
                 <p v-for="(tag, index) in post.tags" :key="index">{{tag.name}}</p>
-                <router-link :to="{ name: 'single-post', params: {slug: post.slug }}">Visualizza Post</router-link>
+                <div class="visualizza">
+                    <router-link :to="{ name: 'single-post', params: {slug: post.slug }}">Visualizza Post</router-link>
+                </div>
             </li>
         </ol>
     </div>
@@ -40,5 +50,39 @@ export default ({
 </script>
 
 <style scoped>
+
+.container{
+    width: 100vw;
+    height: 100vh;
+}
+
+.my_flex{
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 auto;
+}
+
+.box{
+    border: solid 1px black;
+    border-radius: 10px;
+    width: 360px;
+    margin: 6px;
+    padding: 35px;
+    list-style-type: none;
+}
+
+h1, p{
+    text-align: center;
+}
+.visualizza{
+    border: solid 1px black;
+    border-radius: 10px;
+    width: 60%;
+    margin: 0 auto;
+    margin-top: 55px;
+    text-align: center;
+    padding: 10px 2px;
+    background-color: rgba(167, 80, 80, 0.4)
+}
 
 </style>
